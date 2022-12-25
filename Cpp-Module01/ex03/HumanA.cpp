d80e0e100693212846574f392e597e655a0cb5d8
Yonghun Lee <yonghlee@c2r8s3.42seoul.kr>
2022-12-25 14:51:03 +0900

    ex03

--- Cpp-Module01/ex03/HumanA.cpp
+++ Cpp-Module01/ex03/HumanA.cpp
@@ -6,7 +6,29 @@
 /*   By: yonghlee <yonghlee@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/25 14:36:17 by yonghlee          #+#    #+#             */
-/*   Updated: 2022/12/25 14:39:57 by yonghlee         ###   ########.fr       */
+/*   Updated: 2022/12/25 14:49:40 by yonghlee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "HumanA.hpp"
+
+HumanA::HumanA()
+{
+
+}
+
+HumanA::HumanA(std::string _name, std::string _weapon)
+{
+    this->name = _name;
+    this->weapon = _weapon;
+}
+
+HumanA::~HumanA()
+{
+
+}
+
+void HumanA::attack()
+{
+    std::cout << this->name << " attacks with their " << this->weapon << '\n';
+}
--- Cpp-Module01/ex03/HumanA.hpp
+++ Cpp-Module01/ex03/HumanA.hpp
@@ -6,7 +6,7 @@
 /*   By: yonghlee <yonghlee@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/25 14:39:01 by yonghlee          #+#    #+#             */
-/*   Updated: 2022/12/25 14:39:56 by yonghlee         ###   ########.fr       */
+/*   Updated: 2022/12/25 14:50:56 by yonghlee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,10 @@ class HumanA
 {
     private:
         std::string name;
-        Weapon      weapon;
+        std::string weapon;
     public:
         HumanA();
-        HumanA(std::string name, Weapon weapon);
+        HumanA(std::string _name, std::string _weapon);
         ~HumanA();
-        void setWeapon();
         void attack();
 };

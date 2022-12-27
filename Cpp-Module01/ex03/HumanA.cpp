155476fb2ddd30748ec5ec724675aed727caaec2
Yonghun Lee <yonghlee@c2r8s5.42seoul.kr>
2022-12-27 15:48:59 +0900

    c01 fin

--- Cpp-Module01/ex03/HumanA.cpp
+++ Cpp-Module01/ex03/HumanA.cpp
@@ -6,13 +6,13 @@
 /*   By: yonghlee <yonghlee@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/25 14:36:17 by yonghlee          #+#    #+#             */
-/*   Updated: 2022/12/27 10:41:16 by yonghlee         ###   ########.fr       */
+/*   Updated: 2022/12/27 15:11:07 by yonghlee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "HumanA.hpp"
 
-HumanA::HumanA(std::string _name, Weapon &_weapon) : weapon(_weapon), name(_name)
+HumanA::HumanA(std::string _name, Weapon &_weapon) : name(_name), weapon(_weapon)
 {
 
 }
